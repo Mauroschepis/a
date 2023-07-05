94f0d5c8e6ad2d2548cf2a4ebdbbdcb8cc28eb02
mauroschepis <schepismauro@gmail.com>
2023-07-05 11:11:30 -0300

    1231

---
+++ ejercicios.py
@@ -0,0 +1,10 @@
+correcta = int(input ("ingrese el numero de respuestas correctas: " ))
+incorrecta = int(input("ingrese el numero de respueas incorrectas: "))
+blanco = int(input("ingrese el numero de respuestas en blanco: "))
+
+puntuaje= correcta*3+incorrecta*-1
+
+print ("la nota final es", puntuaje) 
+
+
+
