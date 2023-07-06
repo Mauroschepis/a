d32d7c9ee6dccc20c2d2ef391d3ac2e79939df44
mauroschepis <schepismauro@gmail.com>
2023-07-06 10:03:05 -0300

    a

--- Conversor_De_Unidades.py
+++ Conversor_De_Unidades.py
@@ -1,3 +1,3 @@
-kilometro=int(input("Ingrese la cantidad de kilogramos"))
+kilometro=float(input("Ingrese la cantidad de kilogramos"))
 milla=kilometro/1.609
 print("el kilometro ingresado equivale a: ", milla, "millas")
