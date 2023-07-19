50e63d1f3bb566e4ac48e31ef63ba06be4f3b787
Mauroschepis <137304147+Mauroschepis@users.noreply.github.com>
2023-07-19 06:19:45 -0300

    Descuento

---
+++ Descuentos.py
@@ -0,0 +1,15 @@
+print("ingrese el costo")
+costo = int(input())
+print ("ingrese la marca")
+m=input()
+if (costo>=2000) and m == "NOSY":
+    pa = costo *0.90
+    pt = costo *0.95
+elif (costo>=2000) and m != "NOSY":
+    pt = costo *0.90
+elif costo < 2000 and m == "NOSY" :
+    pa = costo*0.95
+    pt = pa + pa*0.20
+elif costo < 2000 and m != "NOSY" :
+    pa = costo*1.20
+print("Usted pagara:", pt, "soles")
