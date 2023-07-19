146860b60dd29250e89f8a967cf4bc32fc60c8bf
Mauroschepis <137304147+Mauroschepis@users.noreply.github.com>
2023-07-19 07:41:02 -0300

    multi

---
+++ Multuplicacion.py
@@ -0,0 +1,9 @@
+print("ingrese 3 numeros")
+N1 = int(input())
+N2 = int(input())
+N3 = int (input())
+if N1 < 0:
+ p= N1*N2*N3
+ print ("el producto de los numeros es: ",p)
+elif N1>0:
+    print("la suma de los numeros es :", N1+N2+N3)
