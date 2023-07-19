3747f0845e28b444467b44cd32f658edc8ed8f5a
Mauroschepis <137304147+Mauroschepis@users.noreply.github.com>
2023-07-19 05:34:25 -0300

    Numeroiguales

---
+++ Numerosiguales.py
@@ -0,0 +1,8 @@
+print("Ingrese 3 numeros")
+a = int(input("Ingrese el primer numero"))
+b = int(input("ingrese el segundo numero"))
+c = int (input("Ingrese el tercer numero"))
+if a+b == c:
+    print("Son iguales")
+else:
+    print("No son iguales")
