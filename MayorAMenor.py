73d5d449ed5acc8b7fddc84a41d969dd1f3b1d6e
Mauroschepis <137304147+Mauroschepis@users.noreply.github.com>
2023-07-19 05:46:57 -0300

    mayor A menor

---
+++ MayorAMenor.py
@@ -0,0 +1,7 @@
+print("ingrese el numero 2 numeros")
+A = int(input("Ingrese el numero A"))
+B = int(input("ingrese el numero B"))
+if A > B:
+    print("El numero ", A, " Es mayor que", B )
+else:
+    print("El numero", B, " Es mayor que",  A)
